55eddd247cce6f90f905e7feaa3f084ed2ad60f0
Jeremy Day <jaday@fb.com>
2019-01-15 11:45:57 -0800

    add lower layer to kbd75:z2oh
    this layer is activated by holding down space, and the current mappings
    map frst (on colemak) to be up-left-down-right. these bindings emulate
    my vim bindings for movement, but they work in any application

--- qmk/z2oh/kbd75_rev2/keymaps/z2oh/keymap.c
+++ qmk/z2oh/kbd75_rev2/keymaps/z2oh/keymap.c
@@ -6,6 +6,7 @@
 #define _COLEMAK 0
 #define _QWERTY 1
 #define _RAISE 2
+#define _LOWER 2
 
 enum keycodes {
   COLEMAK = SAFE_RANGE,
@@ -18,21 +19,21 @@ enum keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK] = LAYOUT(
-    KC_ESC,   KC_F1,         KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,        KC_F12,   KC_DEL,   MO(_RAISE),    KC_BTN1,
-    KC_GRV,   KC_1,          KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,       KC_EQL,   KC_DEL,   KC_BSPC,       KC_HOME,
-    KC_TAB,   KC_Q,          KC_W,     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,       KC_RBRC,  KC_BSLS,                 KC_PGUP,
-    KC_BSPC,  KC_A,          KC_R,     KC_S,     KC_T,     KC_D,     KC_H,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,                           KC_ENT,        KC_PGDN,
-    KC_LSPO,  MO(_RAISE),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,       KC_RSPC,            KC_UP,         KC_END,
-    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
+    KC_ESC,   KC_F1,         KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,        KC_F12,   KC_DEL,   MO(_RAISE),    KC_BTN1,
+    KC_GRV,   KC_1,          KC_2,     KC_3,     KC_4,     KC_5,     KC_6,                 KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,       KC_EQL,   KC_DEL,   KC_BSPC,       KC_HOME,
+    KC_TAB,   KC_Q,          KC_W,     KC_F,     KC_P,     KC_G,     KC_J,                 KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,       KC_RBRC,  KC_BSLS,                 KC_PGUP,
+    KC_BSPC,  KC_A,          KC_R,     KC_S,     KC_T,     KC_D,     KC_H,                 KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,                           KC_ENT,        KC_PGDN,
+    KC_LSPO,  MO(_RAISE),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                 KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,       KC_RSPC,            KC_UP,         KC_END,
+    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   LT(_LOWER, KC_SPC),   KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
   ),
 
   [_QWERTY] = LAYOUT(
-    KC_ESC,   KC_F1,         KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,        KC_F12,   KC_DEL,   MO(_RAISE),    KC_BTN1,
-    KC_GRV,   KC_1,          KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,       KC_EQL,   KC_DEL,   KC_BSPC,       KC_HOME,
-    KC_TAB,   KC_Q,          KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,       KC_RBRC,  KC_BSLS,                 KC_PGUP,
-    KC_BSPC,  KC_A,          KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                           KC_ENT,        KC_PGDN,
-    KC_LSPO,  MO(_RAISE),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,       KC_RSPC,            KC_UP,         KC_END,
-    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
+    KC_ESC,   KC_F1,         KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,        KC_F12,   KC_DEL,   MO(_RAISE),    KC_BTN1,
+    KC_GRV,   KC_1,          KC_2,     KC_3,     KC_4,     KC_5,     KC_6,                 KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,       KC_EQL,   KC_DEL,   KC_BSPC,       KC_HOME,
+    KC_TAB,   KC_Q,          KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,                 KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,       KC_RBRC,  KC_BSLS,                 KC_PGUP,
+    KC_BSPC,  KC_A,          KC_S,     KC_D,     KC_F,     KC_G,     KC_H,                 KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                           KC_ENT,        KC_PGDN,
+    KC_LSPO,  MO(_RAISE),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                 KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,       KC_RSPC,            KC_UP,         KC_END,
+    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   LT(_LOWER, KC_SPC),   KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
   ),
 
   [_RAISE] = LAYOUT(
@@ -42,6 +43,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          _______,          _______,          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  QWERTY ,
          _______,          _______,          _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
          _______,          _______,          _______,                      _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______
+  ),
+
+  [_LOWER] = LAYOUT(
+    _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  KC_UP,    _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+    _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,
+    _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+    _______,  _______,  _______,                       _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______
   ),
 };
 
