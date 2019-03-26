e54b954f439fb10c9dcf138aeda002e247a75298
Jeremy Day <jadaytime@gmail.com>
2019-03-25 18:57:22 -0700

    add colemak switch to laptop mode

--- bin/switch_to_laptop_layout.sh
+++ bin/switch_to_laptop_layout.sh
@@ -6,4 +6,7 @@ xrandr --dpi 192
 # Turn on the laptop monitor.
 xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 
+# Switch to Colemak, since I am probably using the laptop keyboard.
+setxkbmap us -variant colemak
+
 nitrogen --restore
