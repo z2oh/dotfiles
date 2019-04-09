e5ce27aadfef5dadd2fa47ea39e2ac361ea34d2b
Jeremy Day <jadaytime@gmail.com>
2019-04-09 07:57:42 -0700

    update desk monitor layout

--- bin/switch_to_desk_layout.sh
+++ bin/switch_to_desk_layout.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Lower the DPI back from 4k resolution.
+xrandr --dpi 96
+
+
 # Add my ROG PG278Q, which isn't autodetected correctly for some reason
 # ** That was a fun couple nights of debugging! I had to "fix" this right after
 # ** I moved to Mountain View, CA, as my desktop's GPU was fried during the
@@ -10,7 +14,7 @@ xrandr --newmode "2560x1440@60" 241,500 2560 2608 2640 2720 1440 1443 1448 1481 
 xrandr --addmode DP2 2560x1440@60
 
 # Set up both monitors.
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 2560x1440 --pos 1440x624 --rotate normal --output DP2 --mode 2560x1440@60 --pos 0x0 --rotate left
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP2 --mode 2560x1440@60 --pos 0x0 --rotate normal
 
 # Restore the background image.
 nitrogen --restore
