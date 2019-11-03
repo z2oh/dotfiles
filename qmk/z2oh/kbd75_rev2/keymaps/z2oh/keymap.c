c119e38e3d713a2d2698217df38209223b2f30e2
Jeremy Day <jadaytime@gmail.com>
2019-11-03 10:02:34 -0800

    update kbd75 keymap to remove space as layer key
    this was causing some issues with spaces not being registered in normal
    computer operation. i think one of the ctrl class keys will have to be
    used for this role, but that improvement will come at a later date

--- qmk/z2oh/kbd75_rev2/keymaps/z2oh/keymap.c
+++ qmk/z2oh/kbd75_rev2/keymaps/z2oh/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,          KC_W,     KC_F,     KC_P,     KC_G,     KC_J,                 KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,       KC_RBRC,  KC_BSLS,                 KC_PGUP,
     KC_BSPC,  KC_A,          KC_R,     KC_S,     KC_T,     KC_D,     KC_H,                 KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,                           KC_ENT,        KC_PGDN,
     KC_LSPO,  MO(_RAISE),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                 KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,       KC_RSPC,            KC_UP,         KC_END,
-    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   LT(_LOWER, KC_SPC),   KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
+    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   KC_SPC,               KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
   ),
 
   [_QWERTY] = LAYOUT(
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,          KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,                 KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,       KC_RBRC,  KC_BSLS,                 KC_PGUP,
     KC_BSPC,  KC_A,          KC_S,     KC_D,     KC_F,     KC_G,     KC_H,                 KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                           KC_ENT,        KC_PGDN,
     KC_LSPO,  MO(_RAISE),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                 KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,       KC_RSPC,            KC_UP,         KC_END,
-    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   LT(_LOWER, KC_SPC),   KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
+    KC_LCTL,  KC_LGUI,       KC_LALT,                      KC_SPC,   KC_SPC,               KC_SPC,                       KC_RALT,  MO(_RAISE),    KC_RCTL,  KC_LEFT,  KC_DOWN,       KC_RGHT
   ),
 
   [_RAISE] = LAYOUT(
